d405841c75dc7b437cf69a168b57a63eb42de264
lyxose <lyxose1097@sjtu.edu.cn>
2025-07-28 13:47:44 +0800

    add a "fixation" pure beep ahead of each trial

--- function_library_cus/genStream.m
+++ function_library_cus/genStream.m
@@ -1,5 +1,5 @@
 
-function stream = genStream(ITI, cSOA, cFreq, tSOA, tFreq, maxRT, stiD, sampRate, noiseAmp, cueAmp, tgAmp, ramp, noiseSeed)
+function stream = genStream(fixT, ITI, cSOA, cFreq, tSOA, tFreq, maxRT, stiD, sampRate, noiseAmp, cueAmp, tgAmp, ramp, noiseSeed)
 
     if nargin < 13 || isempty(noiseSeed)
         noiseSeed = randi([0, intmax('uint32')], 1, 'uint32'); 
@@ -9,17 +9,21 @@ function stream = genStream(ITI, cSOA, cFreq, tSOA, tFreq, maxRT, stiD, sampRate
     stream = noiseAmp.*(rand(1,round(sampRate*(ITI+sum(cSOA)+tSOA+maxRT))).*2-1);
     rng(originalRNG);  % Maintain global random number consistency
 
+    fixSti = MakeBeep(cFreq,fixT,sampRate).*cueAmp;
     cueSti = MakeBeep(cFreq,stiD,sampRate).*cueAmp;
     tgSti = MakeBeep(tFreq,stiD,sampRate).*tgAmp;
+    tix_len = length(fixSti);
     sti_len = length(cueSti);
-    % add ramp to each beep
+    % add ramp to cue and target beep to avoid burst noise
     ramp_env = ones(1,round(sti_len));   
     ramp_len = round(sampRate*ramp);
     ramp_env(1:ramp_len)=linspace(0,1,ramp_len);
     ramp_env(length(ramp_env)-ramp_len+1:end)=linspace(1,0,sampRate*ramp);
     cueSti = cueSti.*ramp_env;
     tgSti = tgSti.*ramp_env;
-    for c = round(sampRate.*(cumsum(cSOA)+ITI))
+    
+    stream(1:tix_len) = fixSti;
+    for c = round(sampRate.*([0,cumsum(cSOA)]+ITI))
         stream(c+1:c+sti_len) = stream(c+1:c+sti_len)+cueSti;
     end
     t = c+round(sampRate*tSOA);
